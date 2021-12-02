ddb6c32b2054cf3c0b46e050f9df347f41db595c
PaigeKeller <Paige Keller>
2021-12-02 09:20:26 -0600

    Initial Commit

---
+++ Checkers/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Checkers
+//
+//  Created by Paige Keller on 12/2/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
